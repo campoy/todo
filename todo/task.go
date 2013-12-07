318f8e57a16065841279001c482732f07f69bae3
Francesc Campoy <campoy@golang.org>
2013-12-06 17:02:30 -0800

    empty title is not acceptable

--- todo/task.go
+++ todo/task.go
@@ -1,10 +1,15 @@
 package todo
 
+import "fmt"
+
 type Task struct {
 	Title string
 	Done  bool
 }
 
-func NewTask(title string) *Task {
-	return &Task{title, false}
+func NewTask(title string) (*Task, error) {
+	if title == "" {
+		return nil, fmt.Errorf("empty title")
+	}
+	return &Task{title, false}, nil
 }
--- todo/task_test.go
+++ todo/task_test.go
@@ -4,11 +4,21 @@ import "testing"
 
 func TestNewTask(t *testing.T) {
 	title := "learn Go"
-	task := NewTask(title)
+	task, err := NewTask(title)
+	if err != nil {
+		t.Errorf("new task: %v", err)
+	}
 	if task.Title != title {
 		t.Errorf("expected title %q, got %q", title, task.Title)
 	}
 	if task.Done {
 		t.Errorf("new task is done")
+	}
+}
+
+func TestNewTaskEmptyTitle(t *testing.T) {
+	_, err := NewTask("")
+	if err == nil {
+		t.Errorf("task with empty title created")
 	}
 }

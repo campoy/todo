6cce793779186ceabeaab0319652edd1b8a41922
Francesc Campoy <campoy@golang.org>
2013-12-09 12:17:38 -0800

    adding spinner

--- static/todo.js
+++ static/todo.js
@@ -6,6 +6,7 @@
 
 function TaskCtrl($scope, $http) {
   $scope.tasks = [];
+  $scope.working = true;
 
   var logError = function(data, status) {
     console.log('code '+status+': '+data);
@@ -13,19 +14,23 @@ function TaskCtrl($scope, $http) {
 
   var refresh = function() {
     $http.get('/task/').
-      success(function(data) { 
-        $scope.tasks = data.Tasks;
-      }).error(logError);
+      success(function(data) { $scope.tasks = data.Tasks; }).
+      error(logError).
+      then(function() { $scope.working = false; });
   };
 
   $scope.addTodo = function() {
-    $http.post('/task/', {Title: $scope.todoText}).success(refresh).error(logError);
-    $scope.todoText = '';
+    $scope.working = true;
+    $http.post('/task/', {Title: $scope.todoText}).
+      success(refresh).error(logError).
+      then(function() { $scope.todoText = ''; });
   };
 
   $scope.toggleDone = function(task) {
+    $scope.working = true;
     task.Done = !task.Done
-    $http.put('/task/'+task.ID, task).success(refresh).error(logError);
+    $http.put('/task/'+task.ID, task).
+      success(refresh).error(logError);
   };
 
   refresh();

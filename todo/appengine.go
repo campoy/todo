6a9ca1f290f4f93d4e5efc684dc5569c0cd55f70
Francesc Campoy <campoy@golang.org>
2013-12-09 11:18:18 -0800

    added appengine disclaimer

--- todo/appengine.go
+++ todo/appengine.go
@@ -5,6 +5,8 @@
 // +build appengine
 
 // A Google App Engine application providing a web UI for task management.
+// DISCLAIMER: This is not intended to be used, since instances can be
+// restarted at any time and all tasks would be lost.
 package todo
 
 import "github.com/campoy/todo/server"

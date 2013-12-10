8ef38d732939f733f4a37a941244da1779b574b7
Francesc Campoy <campoy@golang.org>
2013-12-09 17:30:36 -0800

    added slides

---
+++ talk/rest.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", addHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, r.Method+" not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req struct{ A, B int }
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	var res struct{ Result int }
+	res.Result = req.A + req.B
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Println(err)
+		http.Error(w, "oops", http.StatusInternalServerError)
+	}
+}
